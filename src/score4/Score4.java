dfd4c1549b60236eac085420500268c11d76d527
user <user@user-PC>
2016-07-01 16:34:04 +0300

    Finished Score4 class

--- src/score4/Score4.java
+++ src/score4/Score4.java
@@ -1,13 +1,60 @@
 package score4;
 
+import java.util.Scanner;
+
 
 public class Score4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int choice;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("**********");
+        System.out.println("* Score4 *");
+        System.out.println("**********");
+        System.out.println();
+        System.out.println("Human plays first!");
+        
+        Board board=new Board();
+        boolean b=true;
+        int depth=5;
+        
+        board.print_array();
+        
+        for(int i=0;i<21;i++)
+        {
+            b=true;
+            while(b)
+            {
+                System.out.println("choose one number between 1-7");
+                choice=sc.nextInt();
+                   
+                if((choice<=7)&&(choice>=1))
+                {
+                    if(!board.isfull(choice-1))
+                    {
+                        b=false;
+                        board.play(choice-1,1);
+                    }
+                }
+            }
+                
+            board.print_array();
+                
+            if(board.final_State==1)
+            {
+                break;//telos paixnidiou, kerdizei o human
+            }
+                
+                
+            Min min=new Min(board.array,depth);
+            board.play(min.position,-1);
+            board.print_array();
+                
+            if(board.final_State==-1)
+            {
+                break;//telos paixnidiou, kerdizei h cpu
+            }
+        }
     }
     
 }
